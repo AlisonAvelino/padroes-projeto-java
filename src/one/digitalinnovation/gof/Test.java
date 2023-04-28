5182cfbff30495e9d6d3052099c339e44a013de1
AlisonAvelino <alisonavelino3133@gmail.com>
2023-04-28 16:25:47 -0300

    Conhecendo o Design Pattern Strategy

--- src/one/digitalinnovation/gof/Test.java
+++ src/one/digitalinnovation/gof/Test.java
@@ -1,9 +1,16 @@
 package one.digitalinnovation.gof;
 
+import one.digitalinnovation.gof.singleton.SingletonEager;
+import one.digitalinnovation.gof.singleton.SingletonLazy;
+import one.digitalinnovation.gof.singleton.SingletonLazyHolder;
+import one.digitalinnovation.gof.strategy.*;
+
 public class Test {
     public static void main(String[] args) {
 
-        //Testes relaciodos ao Design Pattern Sigleton
+        //Testes relaciodos ao Design Pattern
+
+        // Sigleton
 
         SingletonLazy lazy = SingletonLazy.getInstancia();
         System.out.println(lazy);
@@ -22,6 +29,23 @@ public class Test {
         //Verificando a nova instancia
         holder = SingletonLazyHolder.getInstancia();
         System.out.println(holder);
+
+        // Strategy
+
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.mover();
+        robo.setComportamento(defensivo);
+        robo.mover();
+        robo.setComportamento(agressivo);
+
+
+
 
     }
 }
