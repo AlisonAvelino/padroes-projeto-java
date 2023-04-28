4b3fcfe4098d29fe814673fd39234302561a3278
AlisonAvelino <alisonavelino3133@gmail.com>
2023-04-28 17:13:51 -0300

    Conhecendo o Design Pattern Facade

--- src/one/digitalinnovation/gof/Test.java
+++ src/one/digitalinnovation/gof/Test.java
@@ -1,5 +1,6 @@
 package one.digitalinnovation.gof;
 
+import one.digitalinnovation.gof.facade.Facade;
 import one.digitalinnovation.gof.singleton.SingletonEager;
 import one.digitalinnovation.gof.singleton.SingletonLazy;
 import one.digitalinnovation.gof.singleton.SingletonLazyHolder;
@@ -44,7 +45,9 @@ public class Test {
         robo.mover();
         robo.setComportamento(agressivo);
 
-
+        // Facade
+        Facade facade = new Facade();
+        facade.migrarCliente("Avelino", "06524987");
 
 
     }
